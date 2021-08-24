72989911c4243596d850b275caabadf88fa060cb
Letícia Oliveira <oliveiraslc@yahoo.com.br>
2021-08-23 21:36:23 -0300

    finished frontend

--- src/pages/Form/Form.js
+++ src/pages/Form/Form.js
@@ -4,6 +4,10 @@ import styles from './Form.module.css';
 import { Link } from 'react-router-dom';
 
 const Form = () => {
+  const days = Array.from({length: 31}, (_, i) => i + 1);
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const years = Array.from(Array(33).keys()).map(i => 1971 + i);
+
   return (
     <div id={styles.formPage}>
       <div id={styles.container}>
@@ -16,142 +20,138 @@ const Form = () => {
           <form>
             <fieldset>
               <legend>Personal Data</legend>
-              <div className={styles.inputBlock}>
-                <label htmlFor="name">Full Name<small> *</small></label>
-                <input type="text" id="name" name="name" required />
-              </div>
-
-              <div className={styles.inputBlock}>
-                <label htmlFor="position">Intended Position</label>
-                <input type="text" id="position" name="position" className={styles.inputOptional} />
-              </div>   
-
-              <div className={styles.inputBlock}>
-                <label>Birth Date<small> *</small></label>
-                <select required>
-                  <option selected disabled value="">Month</option>
-                  <option value="1">Jan</option>
-                  <option value="2">Feb</option>
-                  <option value="3">Mar</option>
-                  <option value="4">Apr</option>
-                  <option value="5">May</option>
-                  <option value="6">Jun</option>
-                  <option value="7">Jul</option>
-                  <option value="8">Aug</option>
-                  <option value="9">Sep</option>
-                  <option value="10">Oct</option>
-                  <option value="11">Nov</option>
-                  <option value="12">Dec</option>
-                </select>
-                <select required>
-                  <option selected disabled value="">Day</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                  <option value="12">12</option>
-                  <option value="13">13</option>
-                  <option value="14">14</option>
-                  <option value="15">15</option>
-                  <option value="16">16</option>
-                  <option value="17">17</option>
-                  <option value="18">18</option>
-                  <option value="19">19</option>
-                  <option value="20">20</option>
-                  <option value="21">21</option>
-                  <option value="22">22</option>
-                  <option value="23">23</option>
-                  <option value="24">24</option>
-                  <option value="25">25</option>
-                  <option value="26">26</option>
-                  <option value="27">27</option>
-                  <option value="28">28</option>
-                  <option value="29">29</option>
-                  <option value="30">30</option>
-                  <option value="31">31</option>
-                </select>
-                <select required>
-                  <option selected disabled value="">Year</option>
-                  <option value="1">1990</option>
-                  <option value="2">1991</option>
-                  <option value="3">1992</option>
-                  <option value="4">1993</option>
-                  <option value="5">1994</option>
-                  <option value="6">1995</option>
-                  <option value="7">1996</option>
-                  <option value="8">1997</option>
-                  <option value="9">1998</option>
-                  <option value="10">1999</option>
-                  <option value="11">2000</option>
-                  <option value="12">2001</option>
-                  <option value="13">2002</option>
-                  <option value="14">2003</option>
-                </select>              
-              </div> 
-
-              <div className={styles.personal}>
-                <div className={styles.inputBlock}>
-                  <label>Marital Status</label>
-                  <select>
-                    <option selected disabled value="">Select</option>
-                    <option value="1">Single</option>
-                    <option value="2">Married</option>
-                    <option value="3">Divorced</option>
-                    <option value="4">Widower</option>
-                  </select>
+              <div className="row">
+                <div className={`${styles.inputBlock} col-md-6`}>
+                  <label htmlFor="name">Full Name<small> *</small></label>
+                  <input type="text" id="name" name="name" required />
                 </div>
 
-                <div className={styles.inputBlock}>
-                  <label>Gender</label>
-                  <select>
-                    <option selected disabled value="">Select</option>
-                    <option value="1">Male</option>
-                    <option value="2">Female</option>
-                    <option value="3">Rather not answer</option>
+                <div className={`${styles.inputBlock} col-md-6`}>
+                  <label htmlFor="position">Intended Position</label>
+                  <input type="text" id="position" name="position" className={styles.inputOptional} />
+                </div>   
+
+                <div id={styles.birth} className={`${styles.inputBlock} col-md-6`}>
+                  <label>Birth Date<small> *</small></label>
+                  <select defaultValue="" required>
+                    <option disabled value="">Month</option>
+                    {months.map((item, index) =>
+                      <option key={item} value={`${index+1}`}>{item}</option>
+                    )}
                   </select>
+                  <select defaultValue="" required>
+                    <option disabled value="">Day</option>
+                    {days.map(item =>
+                      <option key={item} value={item}>{item}</option>
+                    )}                  
+                  </select>
+                  <select defaultValue="" required>
+                    <option disabled value="">Year</option>
+                    {years.map(item =>
+                      <option key={item} value={item}>{item}</option>
+                    )}                 
+                  </select>              
+                </div> 
+
+                <div className={`${styles.personal} col-md-6`}>
+                  <div className={styles.inputBlock}>
+                    <label>Marital Status</label>
+                    <select defaultValue="">
+                      <option disabled value="">Select</option>
+                      <option value="1">Single</option>
+                      <option value="2">Married</option>
+                      <option value="3">Divorced</option>
+                      <option value="4">Widower</option>
+                    </select>
+                  </div>
+
+                  <div className={styles.inputBlock}>
+                    <label>Gender</label>
+                    <select defaultValue="">
+                      <option disabled value="">Select</option>
+                      <option value="1">Male</option>
+                      <option value="2">Female</option>
+                      <option value="3">Rather not answer</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className={`${styles.inputBlock} col-md-12`}>
+                  <label htmlFor="address">Address<small> *</small></label>
+                  <input type="text" id="address" name="address" required />
+                </div>
+
+                <div className={`${styles.inputBlock} col-md-6`}>
+                  <label htmlFor="neighborhood">Neighborhood<small> *</small></label>
+                  <input type="text" id="neighborhood" name="neighborhood" required />
+                </div>
+
+                <div className={`${styles.inputBlock} col-md-6`}>
+                  <label htmlFor="city">City<small> *</small></label>
+                  <input type="text" id="city" name="city" required />
+                </div>
+
+                <div className={`${styles.inputBlock} col-md-4`}>
+                  <label htmlFor="cep">CEP</label>
+                  <input type="text" id="cep" name="cep" className={styles.inputOptional} />
+                </div>
+
+                <div className={`${styles.inputBlock} col-md-4`}>
+                  <label htmlFor="phone1">Phone 1</label>
+                  <input type="text" id="phone1" name="phone1" className={styles.inputOptional} />
+                </div>
+
+                <div className={`${styles.inputBlock} col-md-4`}>
+                  <label htmlFor="phone2">Phone 2</label>
+                  <input type="text" id="phone2" name="phone2" className={styles.inputOptional} />
+                </div>
+
+                <div className={`${styles.inputBlock} col-md-6`}>
+                  <label htmlFor="cellphone">Cell Phone</label>
+                  <input type="text" id="cellphone" name="cellphone" className={styles.inputOptional} />
+                </div>
+
+                <div className={`${styles.inputBlock} col-md-6`}>
+                  <label htmlFor="email">E-mail</label>
+                  <input type="text" id="email" name="email" className={styles.inputOptional} />
                 </div>
               </div>
             </fieldset>
 
             <fieldset>
               <legend>Documents</legend>
-              <div className={styles.inputBlock}>
-                <label htmlFor="identity">ID<small> *</small></label>
-                <input id="identity" name="identity" required />
-              </div>
-
-              <div className={styles.inputBlock}>
-                <label htmlFor="cpf">CPF<small> *</small></label>
-                <input id="cpf" name="cpf" required />
-              </div>
-
-              <div className={styles.docs}>
-                <div className={styles.inputBlock}>
-                  <label>Vehicle</label>
-                  <select>
-                    <option selected disabled value="">Select</option>
-                    <option value="1">Yes</option>
-                    <option value="2">No</option>
-                  </select>
+              <div className="row">
+                <div className={`${styles.inputBlock} col-md-6`}>
+                  <label htmlFor="identity">ID<small> *</small></label>
+                  <input id="identity" name="identity" required />
                 </div>
 
-                <div className={styles.inputBlock}>
-                  <label>Driving License</label>
-                  <select>
-                    <option selected disabled value="">Select</option>
-                    <option value="1">A</option>
-                    <option value="2">B</option>
-                    <option value="3">C</option>
-                    <option value="4">D</option>
-                    <option value="5">E</option>
-                  </select>
+                <div className={`${styles.inputBlock} col-md-6`}>
+                  <label htmlFor="cpf">CPF<small> *</small></label>
+                  <input id="cpf" name="cpf" required />
+                </div>
+
+                <div className={styles.docs}>
+                  <div className={styles.inputBlock}>
+                    <label>Vehicle</label>
+                    <select defaultValue="">
+                      <option disabled value="">Select</option>
+                      <option value="1">Yes</option>
+                      <option value="2">No</option>
+                    </select>
+                  </div>
+
+                  <div className={styles.inputBlock}>
+                    <label>Driving License</label>
+                    <select defaultValue="">
+                      <option disabled value="">Select</option>
+                      <option value="1">A</option>
+                      <option value="2">B</option>
+                      <option value="3">C</option>
+                      <option value="4">D</option>
+                      <option value="5">E</option>
+                    </select>
+                  </div>
                 </div>
               </div>
             </fieldset>
